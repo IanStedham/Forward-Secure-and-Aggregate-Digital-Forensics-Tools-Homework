5f553228fda3f73b5cf771fbac6c36b78b52277a
Ryan William Powers <191621125+ryypow@users.noreply.github.com>
2026-02-27 05:34:32 +0000

    bob - parse received ciphertext and aggregatehmacs, rebuild aggregate hmac file

--- bob.c
+++ bob.c
@@ -23,10 +23,69 @@ int main(int argc, char *argv[]) {
 
     //read aggregate HMAC from AggregatedHMAC.txt
     int hmacLength;
-    unsigned char* aggregateHMAC = Read_File(argv[3], $hmacLength);
+    unsigned char* aggregateHMACtxt = Read_File(argv[3], $hmacLength);
 
     //create the initial symmetric key k1 with the shared seed
     unsigned char* initialKey = PNRG(sharedSeed, seedLength, 1024);
+
+    //parse the HEX ciphertext messages - one message = 2048 hex chars)
+    unsigned char cipherTexts[10][1024];
+    for (int i = 0;i < 10; i++) {
+        char hexLine[2049];
+        memcpy(hexLine, cipher + i * 2049, 2048);
+        hexLine[2048] = '\0';
+        hex2Byte(cipherTexts[i], hexLine, 1024);
+    }
+
+    //parse aggregateHMACs.txt
+    unsigned char aggregateHMACbyte[32]; //does this 32 hold the whole file? what does it get sent in?
+    char aggregateHMAC_HEX[65];
+    memcpy(aggregateHMAC_HEX, aggregateHMACtxt, 64);
+    hex2Byte(aggregateHMACbyte, aggregateHMAC_HEX, 32);
+
+    //compute hmac
+    unsigned char computeAggregateHMAC[32];
+    unsigned char hmacs[10][32]; //one per cipher
+
+    for (int i = 0;i < 10; i++) {
+        unsigned char *sigma = HMAC_SHA256(initialKey, 32, cipherTexts[i], 1024);
+        memcpy(hmacs[i], sigma, 32);
+        free(sigma);
+
+        //derive next key
+        unsigned char *nextKey = Hash_SHA256(initialKey, 32);
+        free(initialKey);
+        initialKey = nextKey;
+    }
+
+    //build aggregate hmac file
+    unsigned char *aggregateHash = Hash_SHA256(hmacs[0], 32);
+    memcpy(computeAggregateHMAC, aggregateHash, 32);
+    free(aggregateHash);
+
+    for (int i = 0; i < 10; i++);
+        unsigned char concat[64];
+        memcpy(concat, computeAggregateHMAC, 32);
+        memcpy(concat + 32, hmacs[i], 32); 
+        unsigned char *confirmationHash = Hash_SHA256(concat, 64);
+        memcpy(computeAggregateHMAC, confirmationHash, 32);
+        free(confirmationHash);
+
+    if (memcmp(computeAggregateHMAC, aggregateHMACbyte, 32) != 0) {
+        printf("HMACs do not match - data has been tampered");
+        exit(1);
+    }
+    printf("HMACs confirmed. Decrypting now");
+
+    initialKey = PNRG(sharedSeed, seedLength, 32);
+
+    unsigned char plaintexts[10][1025];
+    for (int i=0; i<10; i++) {
+        unsigned char *plaintext = AES_CTR
+    }
+
+
+
 
 }
 
@@ -131,7 +190,23 @@ unsigned char* HMAC_SHA256(unsigned char* key, int keyLength, unsigned char* inp
 {
     unsigned char *HMAC = malloc(SHA256_DIGEST_LENGTH);
 
-    HMAC(EVP_sha256(), key, keyLength, input, inputLength)
+    HMAC(EVP_sha256(), key, keyLength, input, inputLength);
 
     return HMAC;
+}
+//=====
+// AES_decrypt
+// ======
+unsigned char* AES_CTR_Decrypt(unsigned char* key, unsigned char* ciphertext) {
+    unsigned char IV[16] = "abcdefghijklmnop";
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, IV);
+
+    unsigned char *plaintext = malloc(1024 + EVP_MAX_BLOCK_LENGTH);
+    int outlen;
+    EVP_DecryptUpdate(ctx, plaintext, &outlen, ciphertext, 1024);
+    EVP_DecryptFinal_ex(ctx, plaintext + outlen, &outlen);
+
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext;
 }
