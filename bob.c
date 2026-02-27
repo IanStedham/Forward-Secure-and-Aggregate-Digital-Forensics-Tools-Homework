255234652a483d1108263e35fe4a26299e04fafa
Ryan William Powers <191621125+ryypow@users.noreply.github.com>
2026-02-27 06:46:56 +0000

    added functions to utils.c to test for functionality

--- bob.c
+++ bob.c
@@ -13,201 +13,103 @@ unsigned char* Read_File(char fileName[], int *fileLen);
 unsigned char* AES_CTR(unsigned char* key, unsigned char* message);
 unsigned char* HMAC_SHA256(unsigned char* key, int keyLength, unsigned char* input, unsigned long inputLength)
 
+//
 int main(int argc, char *argv[]) {
-    //read the shared seed
+    // Bob reads the shared seed from a file named ”SharedSeed.txt”
     int seedLength;
     unsigned char* sharedSeed = Read_File(argv[1], &seedLength);
 
-    //read ciphertexts from Ciphertexts.txt
+    //Bobreadstheciphertexts from ”Ciphertexts.txt”
     int cipherLength;
-    unsigned char* cipher = Read_File(argv[2], $cipherLength);
+    unsigned char* cipher = Read_File(argv[2], &cipherLength);
 
-    //read aggregate HMAC from AggregatedHMAC.txt
+    //Bobreadsthe the aggregated HMAC from ”AggregatedHMAC.txt”
     int hmacLength;
-    unsigned char* aggregateHMACtxt = Read_File(argv[3], $hmacLength);
+    unsigned char* aggregateHMAC_hex = Read_File(argv[3], &hmacLength);
 
     //create the initial symmetric key k1 with the shared seed
-    unsigned char* initialKey = PNRG(sharedSeed, seedLength, 1024);
+    unsigned char* initialKey = PRNG(sharedSeed, seedLength, 32);
 
-    //parse the HEX ciphertext messages - one message = 2048 hex chars)
+    //parse the HEX ciphertext messages - one cipherHex message = 2048 hex chars)
     unsigned char cipherTexts[10][1024];
     for (int i = 0;i < 10; i++) {
-        char hexLine[2049];
-        memcpy(hexLine, cipher + i * 2049, 2048);
-        hexLine[2048] = '\0';
-        hex2Byte(cipherTexts[i], hexLine, 1024);
+        char cipherText_hex[2049];
+         //each cipher message + newline /n = 2049
+         //to get to line i, jump i * 2049 byutes into the buffer and memcpy the 2048 characters (excluding /n)
+        memcpy(cipherText_hex, cipher + i * 2049, 2048);
+        cipherText_hex[2048] = '\0';
+        hex2Byte(cipherTexts[i], cipherText_hex, 1024); //convert received ciphermessage to bytes
     }
 
-    //parse aggregateHMACs.txt
-    unsigned char aggregateHMACbyte[32]; //does this 32 hold the whole file? what does it get sent in?
-    char aggregateHMAC_HEX[65];
-    memcpy(aggregateHMAC_HEX, aggregateHMACtxt, 64);
-    hex2Byte(aggregateHMACbyte, aggregateHMAC_HEX, 32);
+    //convert received aggregateHMAC sigma_1-i to bytes
+    unsigned char aggregateHMAC_byte_received[32]; 
+    hex2Byte(aggregateHMAC_byte_received, aggregateHMAC_hex, 32);
 
-    //compute hmac
-    unsigned char computeAggregateHMAC[32];
-    unsigned char hmacs[10][32]; //one per cipher
+    //compute the aggregate hmac sigma_1-i
+    unsigned char aggregateHMAC[32];
+    unsigned char hmacs[10][32]; //one per cipher - 10 ciphers
 
     for (int i = 0;i < 10; i++) {
         unsigned char *sigma = HMAC_SHA256(initialKey, 32, cipherTexts[i], 1024);
         memcpy(hmacs[i], sigma, 32);
         free(sigma);
 
-        //derive next key
+        //derive next hmac sigma_i+1
         unsigned char *nextKey = Hash_SHA256(initialKey, 32);
         free(initialKey);
         initialKey = nextKey;
     }
 
     //build aggregate hmac file
-    unsigned char *aggregateHash = Hash_SHA256(hmacs[0], 32);
-    memcpy(computeAggregateHMAC, aggregateHash, 32);
-    free(aggregateHash);
+    unsigned char *aggregateHash_s1 = Hash_SHA256(hmacs[0], 32);
+    memcpy(aggregateHMAC, aggregateHash_s1, 32);
+    free(aggregateHash_s1);
 
-    for (int i = 0; i < 10; i++);
-        unsigned char concat[64];
-        memcpy(concat, computeAggregateHMAC, 32);
+    for (int i = 0; i < 10; i++) {
+        unsigned char concat[64]; //concat = sigma_prev + sigma_current
+        memcpy(concat, aggregateHMAC, 32);
         memcpy(concat + 32, hmacs[i], 32); 
         unsigned char *confirmationHash = Hash_SHA256(concat, 64);
-        memcpy(computeAggregateHMAC, confirmationHash, 32);
+        memcpy(aggregateHMAC, confirmationHash, 32);
         free(confirmationHash);
+    }
 
-    if (memcmp(computeAggregateHMAC, aggregateHMACbyte, 32) != 0) {
+    if (memcmp(aggregateHMAC, aggregateHMAC_byte_received, 32) != 0) {
         printf("HMACs do not match - data has been tampered");
         exit(1);
     }
     printf("HMACs confirmed. Decrypting now");
 
-    initialKey = PNRG(sharedSeed, seedLength, 32);
+    initialKey = PRNG(sharedSeed, seedLength, 32);
 
     unsigned char plaintexts[10][1025];
     for (int i=0; i<10; i++) {
-        unsigned char *plaintext = AES_CTR
+        unsigned char *plaintext = AES_CTR_Decrypt(initialKey, cipherTexts[i]);
+        memcpy(plaintexts[i], plaintext, 1024);
+        plaintexts[i][1024] = '\0';
+        free(plaintext);
+
+        unsigned char *nextKey = Hash_SHA256(initialKey, 32);
+        free(initialKey);
+        initialKey = nextKey;
     }
 
+    unsigned char decrypted_buf[1025 * 10];
+    for (int i=0; i<10; i++) {
+        memcpy(decrypted_buf + i * 1025, plaintexts[i], 1024); //excludes /n***
+        if (i != 9)
+            decrypted_buf[i * 1025 + 1024] = '\n';
+    }
 
-
-
-}
-
-
-/*============================
-        Read from File
-==============================*/
-unsigned char* Read_File (char fileName[], int *fileLen)
-{
-    FILE *pFile;
-	pFile = fopen(fileName, "r");
-	if (pFile == NULL)
-	{
-		printf("Error opening file.\n");
-		exit(0);
-	}
-    fseek(pFile, 0L, SEEK_END);
-    int temp_size = ftell(pFile)+1; //get file size
-    fseek(pFile, 0L, SEEK_SET);
-    unsigned char *output = (unsigned char*) malloc(temp_size); //messageLength variable from main
-	fgets(output, temp_size, pFile);
-	fclose(pFile);
-
-    *fileLen = temp_size-1;
-	return output;
-}
-/*============================
-        Write to File
-==============================*/
-void Write_File(char fileName[], char input[], int input_length){
-  FILE *pFile;
-  pFile = fopen(fileName,"w");
-  if (pFile == NULL){
-    printf("Error opening file. \n");
-    exit(0);
-  }
-  //fputs(input, pFile);
-  fwrite(input, 1, input_length, pFile);
-  fclose(pFile);
-}
-/*============================
-        SHA-256 Fucntion
-==============================*/
-unsigned char* Hash_SHA256(unsigned char* input, unsigned long inputlen)
-{
-    unsigned char *hash = malloc(SHA256_DIGEST_LENGTH);
-
-    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
-    EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
-    EVP_DigestUpdate(ctx, input, inputlen);
-    EVP_DigestFinal_ex(ctx, hash, NULL);
-    EVP_MD_CTX_free(ctx);
-
-    return hash;
-}
-/*============================
-        PRNG Fucntion 
-==============================*/
-unsigned char* PRNG(unsigned char *seed, unsigned long seedlen, unsigned long prnglen)
-{
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    unsigned char *pseudoRandomNumber = malloc(prnglen);
-
-    unsigned char nonce[16] = {0};
-
-    EVP_EncryptInit_ex(ctx, EVP_chacha20(), NULL, seed, nonce);
-
-    unsigned char zeros[prnglen];
-    memset(zeros, 0, prnglen);
-
-    int outlen;
-    EVP_EncryptUpdate(ctx, pseudoRandomNumber, &outlen, zeros, prnglen);
-    EVP_EncryptFinal(ctx, pseudoRandomNumber, &outlen);
-
-    EVP_CIPHER_CTX_free(ctx);
-    return pseudoRandomNumber;
-}
-/*============================
-      AES-CTR Function 
-==============================*/
-unsigned char* AES_CTR(unsigned char* key, unsigned char* message) {
-    unsigned char IV[16] = "abcdefghijklmnop";
-
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    Write_File("Plaintexts.txt", decrypted_buf, 1025 * 10 - 1);
     
-    EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, IV);
+    return 0;
 
-    unsigned char *encryptMessage = malloc(1024);
-
-    int encryptMessageLen;
-    EVP_EncryptUpdate(ctx, encryptMessage, &encryptMessageLen, message, 1024);
-    EVP_EncryptFinal(ctx, encryptMessage, &encryptMessageLen);
-    
-    EVP_CIPHER_CTX_free(ctx);
-    return encryptMessage;
 }
 
-//=====================
-// HMAC-SHA256 FUNCTION 
-//======================
-unsigned char* HMAC_SHA256(unsigned char* key, int keyLength, unsigned char* input, unsigned long inputLength)
-{
-    unsigned char *HMAC = malloc(SHA256_DIGEST_LENGTH);
 
-    HMAC(EVP_sha256(), key, keyLength, input, inputLength);
 
-    return HMAC;
+
+
 }
-//=====
-// AES_decrypt
-// ======
-unsigned char* AES_CTR_Decrypt(unsigned char* key, unsigned char* ciphertext) {
-    unsigned char IV[16] = "abcdefghijklmnop";
-    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-    EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, IV);
 
-    unsigned char *plaintext = malloc(1024 + EVP_MAX_BLOCK_LENGTH);
-    int outlen;
-    EVP_DecryptUpdate(ctx, plaintext, &outlen, ciphertext, 1024);
-    EVP_DecryptFinal_ex(ctx, plaintext + outlen, &outlen);
-
-    EVP_CIPHER_CTX_free(ctx);
-    return plaintext;
-}
--- utils.c
+++ utils.c
@@ -5,8 +5,15 @@
 #include <openssl/ec.h>
 #include <openssl/err.h>
 #include <openssl/sha.h>
+#include <unistd.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
 
-char* Read_File (char fileName[], int *fileLen)
+/*============================
+        Read from File
+==============================*/
+unsigned char* Read_File (char fileName[], int *fileLen)
 {
     FILE *pFile;
 	pFile = fopen(fileName, "r");
@@ -16,42 +23,110 @@ char* Read_File (char fileName[], int *fileLen)
 		exit(0);
 	}
     fseek(pFile, 0L, SEEK_END);
-    int temp_size = ftell(pFile)+1;
+    int temp_size = ftell(pFile)+1; //get file size
     fseek(pFile, 0L, SEEK_SET);
-    char *output = (char*) malloc(temp_size);
+    unsigned char *output = (unsigned char*) malloc(temp_size); //messageLength variable from main
 	fgets(output, temp_size, pFile);
 	fclose(pFile);
 
     *fileLen = temp_size-1;
 	return output;
 }
-
-// Write to file
-void Write_File(char fileName[], char input[]){
+/*============================
+        Write to File
+==============================*/
+void Write_File(char fileName[], char input[], int input_length){
   FILE *pFile;
   pFile = fopen(fileName,"w");
   if (pFile == NULL){
     printf("Error opening file. \n");
     exit(0);
   }
-  fputs(input, pFile);
+  //fputs(input, pFile);
+  fwrite(input, 1, input_length, pFile);
   fclose(pFile);
 }
-
-// Show in hex
-void Show_in_Hex(char name[], unsigned char hex[], int hexlen)
+/*============================
+        SHA-256 Fucntion
+==============================*/
+unsigned char* Hash_SHA256(unsigned char* input, unsigned long inputlen)
 {
-	printf("%s: ", name);
-	for (int i = 0 ; i < hexlen ; i++)
-   		printf("%02x", hex[i]);
-	printf("\n");
+    unsigned char *hash = malloc(SHA256_DIGEST_LENGTH);
+
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_sha256(), NULL);
+    EVP_DigestUpdate(ctx, input, inputlen);
+    EVP_DigestFinal_ex(ctx, hash, NULL);
+    EVP_MD_CTX_free(ctx);
+
+    return hash;
+}
+/*============================
+        PRNG Fucntion 
+==============================*/
+unsigned char* PRNG(unsigned char *seed, unsigned long seedlen, unsigned long prnglen)
+{
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    unsigned char *pseudoRandomNumber = malloc(prnglen);
+
+    unsigned char nonce[16] = {0};
+
+    EVP_EncryptInit_ex(ctx, EVP_chacha20(), NULL, seed, nonce);
+
+    unsigned char zeros[prnglen];
+    memset(zeros, 0, prnglen);
+
+    int outlen;
+    EVP_EncryptUpdate(ctx, pseudoRandomNumber, &outlen, zeros, prnglen);
+    EVP_EncryptFinal(ctx, pseudoRandomNumber, &outlen);
+
+    EVP_CIPHER_CTX_free(ctx);
+    return pseudoRandomNumber;
+}
+/*============================
+      AES-CTR Function 
+==============================*/
+unsigned char* AES_CTR(unsigned char* key, unsigned char* message) {
+    unsigned char IV[16] = "abcdefghijklmnop";
+
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    
+    EVP_EncryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, IV);
+
+    unsigned char *encryptMessage = malloc(1024);
+
+    int encryptMessageLen;
+    EVP_EncryptUpdate(ctx, encryptMessage, &encryptMessageLen, message, 1024);
+    EVP_EncryptFinal(ctx, encryptMessage, &encryptMessageLen);
+    
+    EVP_CIPHER_CTX_free(ctx);
+    return encryptMessage;
 }
 
-// Convert to hex
-void Convert_to_Hex(char output[], unsigned char input[], int inputlength)
+//=====================
+// HMAC-SHA256 FUNCTION 
+//======================
+unsigned char* HMAC_SHA256(unsigned char* key, int keyLength, unsigned char* input, unsigned long inputLength)
 {
-    for (int i=0; i<inputlength; i++){
-        sprintf(&output[2*i], "%02x", input[i]);
-    }
-    //printf("Hex format: %s\n", output);  //remove later
+    unsigned char *HMAC = malloc(SHA256_DIGEST_LENGTH);
+
+    HMAC(EVP_sha256(), key, keyLength, input, inputLength);
+
+    return HMAC;
+}
+//=====
+// AES_decrypt
+// ======
+unsigned char* AES_CTR_Decrypt(unsigned char* key, unsigned char* ciphertext) {
+    unsigned char IV[16] = "abcdefghijklmnop";
+    EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
+    EVP_DecryptInit_ex(ctx, EVP_aes_256_ctr(), NULL, key, IV);
+
+    unsigned char *plaintext = malloc(1024 + EVP_MAX_BLOCK_LENGTH);
+    int outlen;
+    EVP_DecryptUpdate(ctx, plaintext, &outlen, ciphertext, 1024);
+    EVP_DecryptFinal_ex(ctx, plaintext + outlen, &outlen);
+
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext;
 }
